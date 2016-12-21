ea36c9addae834f10215d460cb14866b1e8caa12
Ali <Ali@DESKTOP-AK>
2016-12-21 09:47:05 +0300

    Excersize 2 implement Post and Put initial

--- Read-OnlyWebAPI/Services/ContactRepository.cs
+++ Read-OnlyWebAPI/Services/ContactRepository.cs
@@ -8,13 +8,51 @@ namespace Read_OnlyWebAPI.Services
 {
     public class ContactRepository
     {
-        public Contact[] GetAllContact()
+        private const string CacheKey = "ContactStore";
+
+        public ContactRepository()
         {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                if (ctx.Cache[CacheKey] == null)
+                {
+                    var contacts = new Contact[]
+                    {
+                        new Contact {Id=1,Name="Ali KÖPRÜLÜ" },
+                        new Contact {Id=2,Name="Alper KÖPRÜLÜ" }
+                    };
+                    ctx.Cache[CacheKey] = contacts;
+                }
+            }
+        }
+
+        public Contact[] GetAllContacts()
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                return (Contact[])ctx.Cache[CacheKey];
+            }
             return new Contact[]
             {
-                new Contact {Id=1,Name="Ali KÖPRÜLÜ" },
-                new Contact {Id=2,Name="Alper KÖPRÜLÜ" }
+                new Contact
+                {
+                    Id=0,
+                    Name="Placeholder"
+                }
             };
         }
+
+        //public Contact[] GetAllContact()//Ex01
+        //{
+        //    return new Contact[]
+        //    {
+        //        new Contact {Id=1,Name="Ali KÖPRÜLÜ" },
+        //        new Contact {Id=2,Name="Alper KÖPRÜLÜ" }
+        //    };
+        //}
     }
 }
